d3bff226a2201c7c37bb94fb1b8a008c529c33c3
jl123567 <ledbetterte@gmail.com>
2018-04-03 18:29:47 -0400

    Unicode fix and help added up arrow is not a unicode charater acording to this so i used thumbs up/down added a help command at m!help

--- main.py
+++ main.py
@@ -17,14 +17,21 @@ async def on_message(message):
     if message.content.startswith('m!test'):
         await client.send_message(message.channel, 'hello <:upvote:428244958504550411>')
 
+    if message.content.startswith('m!help'):
+        await client.send_message(message.channel, "```m!startVote | reacts with :up_arrow: and :down_arrow:"
+                                                   " to the channel the command is used in (for the last 100 messages)\n"
+                                                   "m!removeVote | remove the bot's reacts\n"
+                                                   "m!help | this```")
+
+
     if message.content.startswith('m!startVote'):
         async for log in client.logs_from(message.channel, limit=100):
-            await client.add_reaction(log, '⬆️')
-            await client.add_reaction(log, '⬇️')
+            await client.add_reaction(log, u"\U0001F44D")
+            await client.add_reaction(log, u"\U0001F44E")
 
     if message.content.startswith('m!removeVote'):
-        async for log in client.logs_from(message.channel, limit=100):
-            await client.remove_reaction(log, '⬆', client.user)
-            await client.remove_reaction(log, '⬇️', client.user)
+        async for log in client.logs_from(message.channel, limit=200):
+            await client.remove_reaction(log, u"\U0001F44D", client.user)
+            await client.remove_reaction(log, u"\U0001F44E", client.user)
 
 client.run('token')
